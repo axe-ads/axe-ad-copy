11624af53ee430e1014e1e41fca5d6599bc01160
Aki <hi@diveyes.net>
2013-08-02 11:42:13 -0500

    Perhaps a slightly better approach.

--- ad.js
+++ ad.js
@@ -1,3 +1,35 @@
-if (you == understand.this) {
-    get.a.girlfriend;
-}
+(function (win) {
+  "use strict";
+
+  var
+    Person = function (understands, preferredGender, name) {
+      this.understandsProgramming = understands;
+      this.preferredGender = preferredGender;
+      this.name = name;
+    };
+
+  function makeThemYours(name, pronoun) {
+    win.console.log(name + ', will you be my ' + pronoun + '?');
+    // TODO insert API call to a candy delivery service?
+  }
+
+  function findAMate(person) {
+    var pronoun;
+    switch (person.preferredGender) {
+    case 'female':
+      pronoun = 'girlfriend';
+      break;
+    case 'male':
+      pronoun = 'boyfriend';
+      break;
+    default:
+      pronoun = 'partner';
+    }
+
+    if (person.understandsProgramming === true) {
+      makeThemYours(person.name, pronoun);
+    }
+
+  }
+
+})(window);
